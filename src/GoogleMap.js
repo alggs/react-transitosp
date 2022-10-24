701dcbfc2c34d47945f438c333b966764d00f912
Almir Gabriel <almirgabrielgds@gmail.com>
2022-10-23 22:14:18 -0300

    Atualiza posição dos onibus (mock)

--- src/GoogleMap.js
+++ src/GoogleMap.js
@@ -1,36 +1,5 @@
-import { React, useEffect, useState } from 'react'
+import { React, useEffect, useState, useCallback } from 'react'
 import { GoogleMap, useJsApiLoader, Marker, MarkerClusterer } from '@react-google-maps/api';
-
-const apiResponse = [
-  {
-    p: 21705,
-    a: true,
-    ta: "2022-10-23T19:17:07Z",
-    py: -23.486624499999998,
-    px: -46.5684925
-  },
-  {
-    p: 21749,
-    a: true,
-    ta: "2022-10-23T19:17:36Z",
-    py: -23.54384675,
-    px: -46.618070125
-  },
-  {
-    p: 21717,
-    a: true,
-    ta: "2022-10-23T19:18:47Z",
-    py: -23.4819775,
-    px: -46.571059500000004
-  },
-  {
-    p: 21709,
-    a: true,
-    ta: "2022-10-23T19:18:30Z",
-    py: -23.4819775,
-    px: -46.571059500000004
-  }
-];
 
 const containerStyle = {
   width: '1500px',
@@ -43,56 +12,91 @@ const center = {
 };
 
 export default function MyComponent() {
+  let apiResponse = [
+    {
+      p: 21705,
+      a: true,
+      ta: "2022-10-23T19:17:07Z",
+      py: -23.486624499999998,
+      px: -46.5684925
+    },
+    {
+      p: 21749,
+      a: true,
+      ta: "2022-10-23T19:17:36Z",
+      py: -23.54384675,
+      px: -46.618070125
+    },
+    {
+      p: 21717,
+      a: true,
+      ta: "2022-10-23T19:18:47Z",
+      py: -23.4819775,
+      px: -46.571059500000004
+    },
+    {
+      p: 21709,
+      a: true,
+      ta: "2022-10-23T19:18:30Z",
+      py: -23.4819775,
+      px: -46.571059500000004
+    }
+  ];
 
-  const [markersLatLong, setMarkersLatLong] = useState(apiResponse);
+  const [busCoord, setNewGeolocalization] = useState(apiResponse)
 
-  useEffect(() => {
-    setTimeout(() => {
-      const updatedMarkersLatLong = markersLatLong.map((marker) => ({
-        lat: marker.py + 10,
-        lng: marker.px + 10
-      }));
-      setMarkersLatLong(updatedMarkersLatLong);
-    }, 5000);
-  }, []);
+  // -----------------------------------------------------
 
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: "AIzaSyDvDRpIjEhQJaA7wr69Z2CLRJC10E2-3XI"
   })
 
-  const [map, setMap] = React.useState(null)
+  useEffect(() => {
+    setTimeout(() => {
+      const updatedMarkersLatLong = apiResponse.map((marker) => ({
+        py: marker.py + Math.random(),
+        px: marker.px + Math.random()
+      }));  
 
-  const onLoad = React.useCallback(function callback(map) {
+      console.log(updatedMarkersLatLong);
+      setNewGeolocalization(updatedMarkersLatLong)
+    }, 7000);
+  }, [busCoord] );
+
+
+
+  const [map, setMap] = useState(null)
+
+  const onLoad = useCallback(function callback(map) {
     const bounds = new window.google.maps.LatLngBounds(center);
     map.fitBounds(bounds);
     setMap(map)
-    map.setZoom(10);
+    map.setZoom(1);
   }, [])
 
-  const onUnmount = React.useCallback(function callback(map) {
+  const onUnmount = useCallback(function callback(map) {
     setMap(null)
   }, [])
 
+
   return isLoaded ? (
-    <GoogleMap
-      mapContainerStyle={containerStyle}
-      center={center}
-      onLoad={onLoad}
-      onUnmount={onUnmount}
-    >
-      {apiResponse.map((marker) => {
-        return (<Marker
-          key={marker.p}
-          position={{
-            lat: marker.py,
-            lng: marker.px
-          }}
-          icon={"http://maps.google.com/mapfiles/kml/shapes/bus.png"}
-        />)
-      })}
-    </GoogleMap>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+      >
+        {busCoord.map((marker) => {
+          return (<Marker
+            key={marker.p}
+            position={{
+              lat: marker.py,
+              lng: marker.px
+            }}
+            icon={"http://maps.google.com/mapfiles/kml/shapes/bus.png"}
+          />)
+        })}
+      </GoogleMap>
   ) : <></>
 }
-
-// export default React.memo(MyComponent);
